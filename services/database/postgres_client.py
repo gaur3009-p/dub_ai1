be1dacceb6adea46be249fc4c1c95950fb0a1ede
gaur3009-p <gaur3009@gmail.com>
2026-02-03 01:29:16 +0530

    Create postgres_client.py

---
+++ services/database/postgres_client.py
@@ -0,0 +1,19 @@
+import psycopg2
+from config.settings import POSTGRES
+
+class PostgresClient:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host=POSTGRES["host"],
+            port=POSTGRES["port"],
+            database=POSTGRES["db"],
+            user=POSTGRES["user"],
+            password=POSTGRES["password"],
+        )
+        self.conn.autocommit = True
+
+    def execute(self, query, params=None):
+        with self.conn.cursor() as cur:
+            cur.execute(query, params)
+            if cur.description:
+                return cur.fetchall()
