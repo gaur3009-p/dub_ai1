4319e34b5f4e1950f41283d4b004a977d7b193eb
gaur3009-p <gaur3009@gmail.com>
2026-02-04 00:36:15 +0530

    Update conversation_repo.py

--- services/database/conversation_repo.py
+++ services/database/conversation_repo.py
@@ -1,8 +1,18 @@
 from services.database.postgres_client import PostgresClient
 
-db = PostgresClient()
+_db = None
+
+def get_db():
+    global _db
+    if _db is None:
+        _db = PostgresClient()
+    return _db
 
 def save_conversation(input_language, input_text, translated_text):
+    db = get_db()
+    if not db or not db.conn:
+        return
+
     query = """
     INSERT INTO conversations (input_language, input_text, translated_text)
     VALUES (%s, %s, %s)
