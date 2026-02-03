dce6c6ec3d1e1e5485f235862bd58362af613d71
gaur3009-p <gaur3009@gmail.com>
2026-02-03 16:49:00 +0530

    Update redis_client.py

--- services/database/redis_client.py
+++ services/database/redis_client.py
@@ -4,6 +4,9 @@ from config.settings import REDIS
 redis_client = redis.Redis(
     host=REDIS["host"],
     port=REDIS["port"],
-    db=REDIS["db"],
-    decode_responses=True
+    username=REDIS.get("username"),
+    password=REDIS.get("password"),
+    db=REDIS.get("db", 0),
+    decode_responses=True,
+    ssl=REDIS.get("ssl", True),
 )
